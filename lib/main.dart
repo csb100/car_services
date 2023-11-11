5f25b69ea4d5b26ada23463f46b760e18351af98
csb100 <carservicebooking100@gmail.com>
2023-11-11 15:19:40 +0400

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:car_services/Login.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
