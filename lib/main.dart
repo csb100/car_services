c50874c04fa1bf9f6f260d30dc23b0ca140b2d03
csb100 <carservicebooking100@gmail.com>
2023-11-15 00:07:23 +0400

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'InfoHome.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
